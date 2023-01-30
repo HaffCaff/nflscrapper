a1d59781f08cb5af88a54ff5dabd666709ea1626
HaffCaff <AnthonyDEpps@gmail.com>
2023-01-29 21:25:36 -0500

    Init commit

---
+++ scrapper.py
@@ -0,0 +1,58 @@
+# Read-only instance
+import praw as praw
+import pandas as pd
+from IPython.display import display
+from praw.models import MoreComments
+import numpy as np
+import matplotlib.pyplot as plt
+
+reddit_read_only = praw.Reddit(client_id="",  # your client id
+                               client_secret="",  # your client secret
+                               user_agent="")  # your user agent
+# URL of the post
+url = "https://www.reddit.com/r/nfl/comments/10og67k/game_thread_san_francisco_49ers_134_at/"
+
+# Creating a submission object
+submission = reddit_read_only.submission(url=url)
+
+post_comments = []
+words = []
+u_words = []
+counts = []
+keywords = ["niners", "eagles", "purdy", "penalty", "hurts", "mccaffery", "brown", "Bosa", "Johnson", "injuries"]
+
+for comment in submission.comments:
+    if type(comment) == MoreComments:
+        continue
+
+    post_comments.append(comment.body)
+
+for comment in post_comments:
+    res = comment.lower().split()
+    for i in res:
+        words.append(i)
+
+words.sort()
+u_words = set(words)
+
+for i in u_words:
+    counts.append(words.count(i))
+
+res = dict(zip(u_words, counts))
+
+# print({k: v for k, v in sorted(res.items(), key=lambda item: item[1])})
+
+finres = {}
+
+for word in keywords:
+    if word in res.keys():
+        finres[word] = res[word]
+
+print(finres)
+
+names = list(finres.keys())
+values = list(finres.values())
+
+plt.bar(range(len(finres)), values, tick_label=names)
+plt.title("Reddit Comments Buzzwords")
+plt.show()
